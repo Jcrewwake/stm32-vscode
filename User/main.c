2cfb1c1377094fa2480065fe2745535f0dcc572c
longyingwang <2570715747@qq.com>
2025-03-03 00:00:24 +0800

    位置式Pid 定速控制

--- User/main.c
+++ User/main.c
@@ -1,5 +1,7 @@
 #include "stm32f10x.h"                  // Device header
 #include <stdint.h>
+#include <math.h>
+#include <float.h>
 #include "Delay.h"
 #include "OLED.h"
 #include "LED.h"
@@ -23,6 +25,7 @@ int main(void)
     RP_Init();
     Motor_Init();
     Encoder_Init();
+    Serial_Init();
 
     Timer_Init();
 
@@ -30,21 +33,27 @@ int main(void)
     OLED_Update();
 
     while (1) {
-        Key_Num = Get_Key_Num();
-        if (Key_Num & 0x01) {
-            Target += 10;
-        } else if (Key_Num & 0x02) {
-            Target -= 10;
-        } else if (Key_Num & 0x04) {
-            Target = 0;
-        }
-        // Ki = (float)RP_Read(1) * 4 / 4096;
-        // Kp = (float)RP_Read(2) * 4 / 4096;
-        // Kd = (float)RP_Read(3) * 4 / 4096;
-        OLED_Printf(0, 16, OLED_8X16, "Tar: %+04.0f", Target);
-        OLED_Printf(0, 32, OLED_8X16, "Act: %+04.0f", Actual);
-        OLED_Printf(0, 48, OLED_8X16, "Out: %+04.0f", Out);
+        // Key_Num = Get_Key_Num();
+        // if (Key_Num & 0x01) {
+        //     Target += 10;
+        // } else if (Key_Num & 0x02) {
+        //     Target -= 10;
+        // } else if (Key_Num & 0x04) {
+        //     Target = 0;
+        // }
+        Target = (float)RP_Read(4) / 4096 * 300 - 150;
+        Kp = (float)RP_Read(1) / 4096 * 2;
+        Ki = (float)RP_Read(2) / 4096 * 2;
+        Kd = (float)RP_Read(3) / 4096 * 2;
+        OLED_Printf(0, 16, OLED_8X16, "Kp:%4.2f", Kp);
+        OLED_Printf(0, 32, OLED_8X16, "Ki:%4.2f", Ki);
+        OLED_Printf(0, 48, OLED_8X16, "Kd:%4.2f", Kd);
+        OLED_Printf(64, 16, OLED_8X16, "Tar:%+04.0f", Target);
+        OLED_Printf(64, 32, OLED_8X16, "Act:%+04.0f", Actual);
+        OLED_Printf(64, 48, OLED_8X16, "Out:%+04.0f", Out);
+        // OLED_ShowFloatNum(0, 16, Target, 3, 0, OLED_8X16);
         OLED_Update();
+        Serial_Printf("%f,%f,%f\r\n", Target, Actual, Out);
     }
 }
 
@@ -55,13 +64,19 @@ void TIM1_UP_IRQHandler(void)
         Key_Tick();
 
         cnt++;
-        if (cnt >= 30) {
+        if (cnt >= 40) {
             cnt = 0;
             Actual = (float)Encoder_Get();
             
             Error1 = Error0;
             Error0 = Target - Actual;
-            ErrorInt += Error0;
+
+            if (fabs(Ki) > FLT_EPSILON) {
+                ErrorInt += Error0;
+            } else {
+                ErrorInt = 0;
+            }
+
             Out = Kp * Error0 + Ki * ErrorInt + Kd * (Error0 - Error1);
 
             if (Out > 100) {
